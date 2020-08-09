c19c818d0d9d200e5fce2c28e430ef44035186b1
cg <c.gruenberger@web.de>
2020-08-10 00:19:30 +0200

    intermediate commit

--- main/src/circularRun.c
+++ main/src/circularRun.c
@@ -7,6 +7,7 @@
 
 #include "animations.h"
 #include "ledData.h"
+#include "stdbool.h"
 
 #define cycle 10u
 static uint8_t cycleCount = cycle;
@@ -25,6 +26,32 @@ void anim_circularRun1(uint8_t brightness) {
 		led_setLedToColor(index + 40u, 0u, 0u, brightness);
 		led_setLedToColor((index + 41u), 0u, brightness, 0u);
 		led_setLedToColor((index + 42u), brightness, 0u, 0u);
+
+		++index;
+		cycleCount = cycle;
+	} else {
+		--cycleCount;
+	}
+}
+
+void anim_circularRun2(uint8_t brightness) {
+	static uint32_t index = 0u;
+	static bool direction = false;
+	static int8_t dir = 1;
+	if (!cycleCount) {
+
+		led_setAllLedsToUniColors(brightness);
+		led_setLedToColor(index, 0u, 0u, brightness);
+		led_setLedToColor((index + 1u), 0u, brightness, 0u);
+		led_setLedToColor((index + 2u), brightness, 0u, 0u);
+		led_setLedToColor(index + 20u, 0u, 0u, brightness);
+		led_setLedToColor((index + 21u), 0u, brightness, 0u);
+		led_setLedToColor((index + 22u), brightness, 0u, 0u);
+		led_setLedToColor(index + 40u, 0u, 0u, brightness);
+		led_setLedToColor((index + 41u), 0u, brightness, 0u);
+		led_setLedToColor((index + 42u), brightness, 0u, 0u);
+
+
 
 		++index;
 		cycleCount = cycle;
