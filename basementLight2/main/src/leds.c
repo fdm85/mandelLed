c8df574a5cd753b10491d754cdd3f4a103b36f65
cg <cg@mstr.green>
2025-11-22 17:37:25 +0100

    fixed led count to what's really needed

--- basementLight2/main/src/leds.c
+++ basementLight2/main/src/leds.c
@@ -10,7 +10,8 @@
 #include "leds.h"
 #include "tim.h"
 
-#define MAIN_LDCNT  750uL
+#define MAIN_R_LDCNT  530uL
+#define MAIN_L_LDCNT  532uL
 #define MRTX_LDCNT  256uL
 #define RAW_LDCNT  16uL
 #define RAW_DMA_CNT (3uL*8uL*RAW_LDCNT)
@@ -19,19 +20,19 @@ static LedRaw rawLeds1[RAW_LDCNT];
 static LedRaw rawLeds2[RAW_LDCNT];
 static LedRaw rawLeds3[RAW_LDCNT];
 lRawDma_t matrix_dma = {.ledCount = MRTX_LDCNT, .rawCount = RAW_LDCNT, .lRaw = rawLeds1, .rawTxCount = RAW_DMA_CNT};
-lRawDma_t mainL_dma = {.ledCount = MAIN_LDCNT, .rawCount = RAW_LDCNT, .lRaw = rawLeds2, .rawTxCount = RAW_DMA_CNT};
-lRawDma_t mainR_dma = {.ledCount = MAIN_LDCNT, .rawCount = RAW_LDCNT, .lRaw = rawLeds3, .rawTxCount = RAW_DMA_CNT};
+lRawDma_t mainL_dma = {.ledCount = MAIN_L_LDCNT, .rawCount = RAW_LDCNT, .lRaw = rawLeds2, .rawTxCount = RAW_DMA_CNT};
+lRawDma_t mainR_dma = {.ledCount = MAIN_R_LDCNT, .rawCount = RAW_LDCNT, .lRaw = rawLeds3, .rawTxCount = RAW_DMA_CNT};
 
-Led_progColor_t mainL_r3[MAIN_LDCNT] __attribute__ ((section (".bssmram"))) = {0};
-diffRunnerCtx_t mainL_diff __attribute__ ((section (".ccmram"))) = {.lDc = &mainL_r3[0], .size = MAIN_LDCNT};
+Led_progColor_t mainL_r3[MAIN_L_LDCNT] __attribute__ ((section (".bssmram"))) = {0};
+diffRunnerCtx_t mainL_diff __attribute__ ((section (".ccmram"))) = {.lDc = &mainL_r3[0], .size = MAIN_L_LDCNT};
 
-Led_progColor_t mainR_r3[MAIN_LDCNT] __attribute__ ((section (".bssmram"))) = {0};
-diffRunnerCtx_t mainR_diff __attribute__ ((section (".ccmram"))) = {.lDc = &mainR_r3[0], .size = MAIN_LDCNT};
+Led_progColor_t mainR_r3[MAIN_R_LDCNT] __attribute__ ((section (".bssmram"))) = {0};
+diffRunnerCtx_t mainR_diff __attribute__ ((section (".ccmram"))) = {.lDc = &mainR_r3[0], .size = MAIN_R_LDCNT};
 
-lLogicContainer(mainL, MAIN_LDCNT);
-lRawContainer(mainL, MAIN_LDCNT);
-lLogicContainer(mainR, MAIN_LDCNT);
-lRawContainer(mainR, MAIN_LDCNT);
+lLogicContainer(mainL, MAIN_L_LDCNT);
+lRawContainer(mainL, MAIN_L_LDCNT);
+lLogicContainer(mainR, MAIN_R_LDCNT);
+lRawContainer(mainR, MAIN_R_LDCNT);
 
 lLogicContainer(matrix, MRTX_LDCNT);
 lRawContainer(matrix, MRTX_LDCNT);
