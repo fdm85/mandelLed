2bcb788432d629d11c417b4e104c3acdbf1568d9
cg <c.gruenberger@web.de>
2025-12-12 13:42:58 +0100

    added basic parser implementation

--- espressif/restful_server/main/uart_tasks.c
+++ espressif/restful_server/main/uart_tasks.c
@@ -14,10 +14,36 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "string.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/_intsup.h>
+#include "BridgeParser.h"
 
-static const int RX_BUF_SIZE = 127;
+typedef struct lSGen_t
+{
+  uint32_t tInt;
+  uint32_t bMult;
+  uint32_t bDiv;
+  uint32_t lStart;
+  uint32_t lEnd;
+}LSGen;
 
-#define UART_TASK_STACK_SIZE (2*2048)
+
+
+typedef struct ringBuff_t
+{
+  char *pl; /**< pointer to start of payload buffer */
+  const uint16_t size; /**< total count of max payload possible */
+  uint16_t r; /**< current read pointer */
+  uint16_t w; /**< current write pointer */
+  uint16_t pad; /**< padding */
+}RingBuff;
+
+static const int RX_BUF_SIZE = 128;
+static const char *RX_TASK_TAG = "RX_TASK";
+static const char *TX_TASK_TAG = "TX_TASK";
+static const TickType_t t2w = 2000 / portTICK_PERIOD_MS;
+#define UART_TASK_STACK_SIZE (2 * 2048)
 
 static void init(void) {
 	const uart_config_t uart_config = {
@@ -29,11 +55,11 @@ static void init(void) {
 		.source_clk = UART_SCLK_DEFAULT,
 	};
 	// We won't use a buffer for sending data.
-	ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, RX_BUF_SIZE * 2, 0, 0, NULL, 0));
+	ESP_ERROR_CHECK(
+		uart_driver_install(UART_NUM_2, RX_BUF_SIZE * 2u, 0, 0, NULL, 0));
 	ESP_ERROR_CHECK(uart_set_mode(UART_NUM_2, UART_MODE_UART));
 	ESP_ERROR_CHECK(uart_param_config(UART_NUM_2, &uart_config));
-	ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 47, 48, UART_PIN_NO_CHANGE,
-								 UART_PIN_NO_CHANGE));
+	ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 47, 48, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 }
 
 static int sendData(const char *logName, const char *data) {
@@ -44,38 +70,47 @@ static int sendData(const char *logName, const char *data) {
 }
 
 static void tx_task(void *arg) {
-	static const char *TX_TASK_TAG = "TX_TASK";
-
 	while (1) {
-		sendData(TX_TASK_TAG, "g 0 r");
-		vTaskDelay(2000 / portTICK_PERIOD_MS);
+		sendData(TX_TASK_TAG, "\r0?\n");
+		vTaskDelay(t2w);
 	}
 }
 
+
 static void rx_task(void *arg) {
-	static const char *RX_TASK_TAG = "RX_TASK";
-	uint8_t *data = (uint8_t *)malloc(RX_BUF_SIZE + 1);
+	static pb_Ctx pb;
+  int inc;
+  pb_ParserState parRes = pb_eIdle;
+  bp_Init(&pb, 10u,RX_BUF_SIZE);
+  
 	while (1) {
-		const int rxBytes = uart_read_bytes(UART_NUM_2, data, 1,
-											1000 / portTICK_PERIOD_MS);
-											(void)rxBytes;
-		if (rxBytes > 0) {
-			data[rxBytes] = 0;
-			ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
-		}
+    inc = uart_read_bytes(UART_NUM_2, &pb.pl[pb.wr], 1, t2w);
+
+    if(inc) {
+      bp_Fill(&pb, inc);
+      parRes = bp_Parse(&pb);
+    }
+    
+    if(parRes == pb_eRx){
+        ESP_LOGW("uart_rx", "received %d bytes, s: s%", pb.wr - pb.rd, pb.pl);
+        pb.rd = pb.wr;
+      }
+      
 	}
-	free(data);
 }
 
 esp_err_t uart_main(void) {
 	init();
-	BaseType_t RxInitSucc = xTaskCreate(rx_task, "uart_rx_task", UART_TASK_STACK_SIZE, NULL,	configMAX_PRIORITIES - 1, NULL);
+	BaseType_t RxInitSucc =
+		xTaskCreate(rx_task, "uart_rx_task", UART_TASK_STACK_SIZE, NULL,
+					configMAX_PRIORITIES - 1, NULL);
 	if (!RxInitSucc)
 		ESP_LOGE("uart_rx", "Create uart_rx task failed");
-	BaseType_t TxInitSucc = xTaskCreate(tx_task, "uart_tx_task", UART_TASK_STACK_SIZE, NULL,
+	BaseType_t TxInitSucc =
+		xTaskCreate(tx_task, "uart_tx_task", UART_TASK_STACK_SIZE, NULL,
 					configMAX_PRIORITIES - 2, NULL);
 	if (!TxInitSucc)
 		ESP_LOGE("uart_tx", "Create uart_tx task failed");
-//	return (RxInitSucc && TxInitSucc) ? ESP_OK : ESP_FAIL;
-	return ESP_OK;
+  
+	return (RxInitSucc && TxInitSucc) ? ESP_OK : ESP_FAIL;
 }
