d11a8ad9bb25858d04a8e7daa0cfa9841364fa91
cg <c.gruenberger@web.de>
2026-03-26 20:49:54 +0100

    fixed some issues in uart implementation

--- MandaLedCtrl_F407VET6/main/src/comPrj.c
+++ MandaLedCtrl_F407VET6/main/src/comPrj.c
@@ -9,6 +9,6 @@
 
 char rxBuf[RX_BUF_SZ];
 char txBuf[TX_BUF_SZ];
-pb_Ctx rxCtx = {.pl = rxBuf, .hwCtx = USART1, .sz = RX_BUF_SZ, .to = 5000uL};
-pb_Ctx txCtx = {.pl = txBuf, .hwCtx = USART1, .sz = TX_BUF_SZ};
+pb_Ctx rxCtx = {.pl = rxBuf, .hwCtx = &huart1, .sz = RX_BUF_SZ, .to = 5000uL};
+pb_Ctx txCtx = {.pl = txBuf, .hwCtx = &huart1, .sz = TX_BUF_SZ};
 
